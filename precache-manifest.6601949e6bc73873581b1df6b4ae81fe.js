3654a0d37c7961f5cd82dded74f927bace2c71fd
Océane Burel <oceanebrl.pro@gmail.com>
2023-02-11 12:16:27 +0100

    mobile version

---
+++ precache-manifest.6601949e6bc73873581b1df6b4ae81fe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2709ff1eb92f50442b4",
+    "url": "/LaMaisonJungle/static/css/main.78efa423.chunk.css"
+  },
+  {
+    "revision": "b2709ff1eb92f50442b4",
+    "url": "/LaMaisonJungle/static/js/main.b2709ff1.chunk.js"
+  },
+  {
+    "revision": "563694ed5d313d746b54",
+    "url": "/LaMaisonJungle/static/js/1.563694ed.chunk.js"
+  },
+  {
+    "revision": "edd153054964cae164fa",
+    "url": "/LaMaisonJungle/static/js/runtime~main.edd15305.js"
+  },
+  {
+    "revision": "3b86648d45636f079e017ecb9d9600df",
+    "url": "/LaMaisonJungle/static/media/monstera.3b86648d.jpg"
+  },
+  {
+    "revision": "f73e0a8633b527bd76daf9d482470ef0",
+    "url": "/LaMaisonJungle/static/media/lyrata.f73e0a86.jpg"
+  },
+  {
+    "revision": "cc60210cc4057a8d2f9d5e095b811006",
+    "url": "/LaMaisonJungle/static/media/pothos.cc60210c.jpg"
+  },
+  {
+    "revision": "463c10225562347d90047a8078df1f96",
+    "url": "/LaMaisonJungle/static/media/succulent.463c1022.jpg"
+  },
+  {
+    "revision": "40fafdd8eec0cce1ecb3e3a91a79fb67",
+    "url": "/LaMaisonJungle/static/media/olivier.40fafdd8.jpg"
+  },
+  {
+    "revision": "dc0510aaa36be864459c2f014e70e006",
+    "url": "/LaMaisonJungle/static/media/basil.dc0510aa.jpg"
+  },
+  {
+    "revision": "14cf9205eb71f94af06c2a8c123d81b5",
+    "url": "/LaMaisonJungle/static/media/mint.14cf9205.jpg"
+  },
+  {
+    "revision": "ff132166b87299887df6b434780e07e8",
+    "url": "/LaMaisonJungle/static/media/calathea.ff132166.jpg"
+  },
+  {
+    "revision": "de9d7400c9eabc647fb0a10845b61ae3",
+    "url": "/LaMaisonJungle/static/media/cactus.de9d7400.jpg"
+  },
+  {
+    "revision": "dbda652cf00285b28df522d3d72ff3b0",
+    "url": "/LaMaisonJungle/static/media/sun.dbda652c.svg"
+  },
+  {
+    "revision": "01fd98f09feb17e58392cb1973b45c82",
+    "url": "/LaMaisonJungle/static/media/water.01fd98f0.svg"
+  },
+  {
+    "revision": "365814caeabbd2866cf6677e18402be2",
+    "url": "/LaMaisonJungle/index.html"
+  }
+];
